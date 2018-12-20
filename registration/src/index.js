2ee198df2610e69cbb822f30b4427646a071c6aa
Apelsin16 <apelsin@i.ua>
2018-12-20 13:57:36 +0200

    'ready'

--- registration/src/index.js
+++ registration/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store';
+
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
+
         <BrowserRouter basename="/registration/build">
             <App />
         </BrowserRouter>
-    </Provider>
+
 , document.getElementById('root'));
