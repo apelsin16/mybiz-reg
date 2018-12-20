3e81843db277f48c925231bc9183b150dc1caaa9
Apelsin16 <apelsin@i.ua>
2018-12-20 12:51:51 +0200

    'ready'

--- registration/src/index.js
+++ registration/src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/registration/build">
             <App />
         </BrowserRouter>
     </Provider>
--- registration/src/redux/actions/tov.js
+++
@@ -1,4 +0,0 @@
-export const add =  val  => ({
-    type: "ADD",
-    payload: val
-});
