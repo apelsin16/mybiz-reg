b3a71b9dfe9c8f58dcb33ee48d92d31f14ef11cb
Apelsin16 <apelsin@i.ua>
2018-12-20 14:15:52 +0200

    'ready'

---
+++ registration/build/precache-manifest.3df1e032a22be4697b6efe8a4756a348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5362ee099502faaed8d3",
+    "url": "/mybiz-reg/registration/build/static/js/runtime~main.5362ee09.js"
+  },
+  {
+    "revision": "a40e04cc5c356b787251",
+    "url": "/mybiz-reg/registration/build/static/js/main.a40e04cc.chunk.js"
+  },
+  {
+    "revision": "2a81afb7dbd96a3e60a1",
+    "url": "/mybiz-reg/registration/build/static/js/1.2a81afb7.chunk.js"
+  },
+  {
+    "revision": "a40e04cc5c356b787251",
+    "url": "/mybiz-reg/registration/build/static/css/main.4b114a2a.chunk.css"
+  },
+  {
+    "revision": "334a0361e5590cafa3780f6a304f02b9",
+    "url": "/mybiz-reg/registration/build/index.html"
+  }
+];
